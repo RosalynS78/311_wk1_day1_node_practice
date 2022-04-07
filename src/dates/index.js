406b08e3f3a124af982d76f2bc702fd4768e0a29
RosalynS78 <rrcervania78@gmail.com>
2022-04-06 23:14:18 -0500

    all passing

--- src/dates/index.js
+++ src/dates/index.js
@@ -1,17 +1,25 @@
 // import moment here; use this package in each function
+const moment = require('moment'); 
 
 const today = () => {
   // write code for dates.today
+let todayDate = moment().format('dddd');
+return todayDate;
 
 }
 
 const calendar = () => {
   // write code for dates.calendar
-
+let calendarDate = moment().format('MMM DD, YYYY'); 
+// month abrviated to three letters
+return calendarDate;
 }
 
 const currentTime = () => {
   // write code for dates.currentTime
+  let thetime = moment().format('hh:mm:ss A');
+  // shows time with a 0 before the first number
+  return thetime;
 
 }
 
--- src/strings/index.js
+++ src/strings/index.js
@@ -1,15 +1,21 @@
 const split = (str, delim) => {
   // write code for strings.split
-
+let newString = str.split(delim);
+// creates new variable that splits string
+return newString
 }
 
 const pairs = (str) => {
   // write code for strings.pairs
+  let arr = str.match(/..?/g); 
+  // g is global will return all items that match 
+  return arr;
 
 }
 
 const reverse = (str) => {
   // write code for strings.reverse
+  return str.split("").reverse().join("");
 
 }
 
